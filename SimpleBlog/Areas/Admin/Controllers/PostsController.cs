95fcaef110a3ec823cacb2ac124e29e486859ad9
M. Barış ÖZ <CPT.7483@gmail.com>
2018-03-09 22:10:22 +0300

    Deneme

---
+++ SimpleBlog/Areas/Admin/Controllers/PostsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Areas.Admin.Controllers
+{
+    public class PostsController : Controller
+    {
+        // GET: Admin/Posts
+        public ActionResult Index()
+        {
+            return Content("Admin Posts Controller");
+        }
+
+        public ActionResult New()
+        {
+            return Content("A new page");
+        }
+
+        public ActionResult Edit(string id)
+        {
+            return Content(id);
+        }
+
+    }
+}
